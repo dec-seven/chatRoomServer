48b9db57ff0ebe5000df548ff83e8fdbc6944f7d
itw_yuwb <itw_yuwb@tkmail.com>
2023-06-14 14:45:04 +0800

    为了解决跨域问提

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const socketio = reqire('socket.io')(http,{
 // 通过socketio监听客户端发送的请求
 socketio.on('contenction',(socket)=>{
   console.log("有人来了",socket.id);
+  // 监听
+  socket.on('textmsg',(data)=>{
+    console.log("客户端说：",data);
+    // 向客户端发送消息
+    socket.emit('textmsg','服务端在看着你呢')
+  })
 })
 
 // 启动http服务器，监听3000端口
