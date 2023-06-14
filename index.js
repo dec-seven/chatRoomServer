661ae376592e2bc80df3525b129e6bdc479b388f
itw_yuwb <itw_yuwb@tkmail.com>
2023-06-14 15:34:22 +0800

    koa

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-// 创建服务器用于监听端口 接受请求
- const http = require('http').createServer()
-// 加载socket模块，获取socket.io对象
-const socketio = require('socket.io')(http,{
+const koa = require('koa') 
+const app = new koa()
+
+const server = app.listen(3000,()=>{
+  console.log("服务启动成功...");
+})
+
+const socketio = require('socket.io')(server,{
   cors:{
     origin:'*'
   }
 })
 
-// 通过socketio监听客户端发送的请求
 socketio.on('connection',(socket)=>{
-  console.log("有人来了",socket.id);
-  // 监听
-  socket.on('textmsg',(data)=>{
-    console.log("客户端说：",data);
-    // 向客户端发送消息
-    socket.emit('textmsg','服务端在看着你呢')
+  console.log("连接成功",socket.id);
+  socket.on('chatMessage',(message)=>{
+    console.log("接收到客户端消息",message);
+    // 指定发送
+    // socket.connected([socket.id]).emit('chatMessage',message.value)
+    // 广播
+    console.log(socket);
+    socket.emit('chatMessage','给客户端返回消息')
+  })
+
+  // 关闭链接的时候执行
+  socket.on('disconnect',()=>{
+    console.log("有人关闭了");
   })
 })
 
-// 启动http服务器，监听3000端口
-http.listen(3000, ()=>{
-  console.log("服务器已启动...");
-})
